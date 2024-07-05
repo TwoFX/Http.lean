c9999e57a4e01ca1f8e2605d808c265db8d38e21
Markus Himmel <markus@lean-fro.org>
2024-07-05 15:41:43 +0200

    Fool around

--- lakefile.lean
+++ lakefile.lean
@@ -23,3 +23,36 @@ require Time from git "https://github.com/axiomed/Time.lean.git"
 
 meta if get_config? env = some "dev" then
   require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
+open Lean
+
+def mkArrayLit (lvl : Level) (type : Expr) (l : List Expr) : Expr :=
+  let empty := Expr.app (Expr.const ``Array.empty [lvl]) type
+  let push r h := mkAppN (Expr.const ``Array.push [lvl]) #[type, r, h]
+  l.foldl push empty
+
+def elabRunPkgConfig (stx : Syntax) (args : Array String) : Elab.TermElabM Expr := do
+  Lean.withRef stx do
+    match ← (IO.Process.output { cmd := "pkg-config", args }).toBaseIO with
+    | .ok out =>
+      if out.exitCode != 0 then
+        throwErrorAt stx "pkg-config failed: {out.exitCode}"
+      let libParts := out.stdout.splitOn
+      let stringType := Expr.const ``String []
+      libParts
+          |>.map (mkStrLit ·.trimRight)
+          |> mkArrayLit .zero stringType
+          |> pure
+    | .error _ =>
+        throwErrorAt stx "Could not run pkg-config"
+
+syntax:lead (name := libuvLibsElab) "libuvLibs" : term
+
+@[term_elab libuvLibsElab]
+def elabLibUVLibs : Lean.Elab.Term.TermElab := fun stx _expectedType? =>
+  elabRunPkgConfig stx #["--libs", "libuv"]
+
+@[default_target]
+lean_exe exampl where
+  root := `Example
+  moreLinkArgs := libuvLibs
